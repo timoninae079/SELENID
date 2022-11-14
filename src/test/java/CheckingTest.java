ada7cd706096722e6d4387258c578e10eb7ae393
ElenaTimonina <timoninae079@gmail.com>
2022-11-14 22:04:49 +0300

    14

---
+++ src/test/java/CheckingTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class CheckingTest {
+  @Test
+    public void positiveTest() {
+        open("http://localhost:9999");
+    }
+}
